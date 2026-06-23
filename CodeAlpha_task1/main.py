0651ee3a0dac4588f87e203758dec5451598c99a
rawwchit <rachitsai2@gmail.com>
2026-06-23 15:43:41 +0530

    Logistic Regression

--- CodeAlpha_task1/main.py
+++ CodeAlpha_task1/main.py
@@ -21,10 +21,15 @@ df["SPEND_RATIO"] = df["TOTAL_SPEND"]/df["INCOME"]
 x = df.drop("DEFAULT", axis = 1)
 y = df.drop("DEFAULT")
 #split
-from sklearn.model_selection import train_test_spit
+from sklearn.model_selection import train_test_split
 x_train, x-test, y_train, y_test = train_test_split(
     x,
     y,
     test_code = 0.2,
     random_state = 42,
 )
+
+# First Model Training
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(x_train,y_train)
