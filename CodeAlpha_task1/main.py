2dd19a94d48b97bc81a95716a3ac313e244bf96b
rawwchit <rachitsai2@gmail.com>
2026-06-23 14:49:28 +0530

    Data exploration -> FE -> Data Preparation for learning.

---
+++ CodeAlpha_task1/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+df = pd.read_csv("credit_data.csv")
+#Data exploration
+print(df.head()) 
+print(df.shape) 
+# print(df.isnull().sum())
+
+# Understanding Target
+count = df['DEFAULT'].value_counts()
+print(count)
+print("\nPercentage : ",df['DEFAULT'].value_counts(normalize=True)*100)
+ratio = (count[0]/count[1])
+print("\nRatio : ",ratio)
+
+# Feature Engineering
+df["DTI"] = df["DEBT"]/df["INCOME"]
+df["SAVINGS_RATIO"] = df["SAVINGS"]/df["INCOME"]
+df["SPEND_RATIO"] = df["TOTAL_SPEND"]/df["INCOME"]
+
+# Data Preparation
+x = df.drop("DEFAULT", axis = 1)
+y = df.drop("DEFAULT")
+
+from sklearn.model_selection import train_test_spit
