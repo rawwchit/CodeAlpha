332bac9846987df0947a01a5ccebbfeb8621dfe2
rawwchit <rachitsai2@gmail.com>
2026-06-26 13:49:18 +0530

    preprocessing

--- CodeAlpha_task1/src/preprocessing.py
+++ CodeAlpha_task1/src/preprocessing.py
@@ -1,5 +1,6 @@
 # Data Preprocessing 
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -13,7 +14,16 @@ def preprocess(df):
 
     # Convert categorical variables into numerical form
     x = pd.get_dummies(x, drop_first=True)
-
+    
+    # Replace infinities with NaN
+    x.replace([np.inf, -np.inf], np.nan, inplace=True)
+    # Fill missing values
+    x.fillna(0, inplace=True)
+    # Data quality check
+    print("\nData Quality Check")
+    print("NaNs :", x.isna().sum().sum())
+    print("Infs :",np.isinf(x.select_dtypes(include=np.number)).sum().sum()
+)
     # Split data
     x_train, x_test, y_train, y_test = train_test_split(
         x,
@@ -29,4 +39,10 @@ def preprocess(df):
     x_train = scaler.fit_transform(x_train)
     x_test = scaler.transform(x_test)
     
-    return (x_train, x_test, y_train, y_test)
+    return (
+        x_train,
+        x_test,
+        y_train,
+        y_test,
+        scaler
+        )
