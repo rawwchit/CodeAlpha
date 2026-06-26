260299734c1380989617bd5975322b4c52ec48b8
rawwchit <rachitsai2@gmail.com>
2026-06-26 13:37:01 +0530

    evaluation part

--- CodeAlpha_task1/src/evaluate.py
+++ CodeAlpha_task1/src/evaluate.py
@@ -7,3 +7,13 @@ from sklearn.metrics import confusion_matrix
 
 def evaluate_model(model,x_test,y_test):
     pred = model.predict(x_test)
+    print()
+    print("Accurcay:",accuracy_score(y_test, pred))
+    print("Precision:",precision_score(y_test, pred))
+    print("Recall:",recall_score(y_test, pred))
+    print("F1 Score:",f1_score(y_test, pred))
+    print("ROC AUC:",roc_auc_score(y_test, pred))
+    print()
+    print("Confusion Matrix")
+    print(confusion_matrix(y_test, pred))
+    
